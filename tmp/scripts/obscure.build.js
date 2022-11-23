3f233090056bcee664b20cd1aabe65c06ce6565e
Joseph Pinder <hello@josephpinder.com>
2022-11-23 16:14:42 -0500

    dist added

---
+++ tmp/scripts/obscure.build.js
@@ -0,0 +1,1 @@
+var newNode;document.querySelectorAll("[obscure]").forEach((function(e){for(var n in(newNode=document.createElement("span")).innerHTML=e.innerHTML,e.classList&&(newNode.classList=e.classList),e.id&&(newNode.id=e.id),e.dataset)newNode.innerHTML=newNode.innerHTML.replaceAll("%"+n.replace("p",""),e.dataset[n]);e.parentNode.insertBefore(newNode,e),e.remove()}));
